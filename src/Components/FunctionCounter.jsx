79a54c5371b4b91d3b389595cb74a3b5b0874026
FREEDOM <freewhynane62@gmail.com>
2021-06-22 13:36:14 +0000

    add FunctionCounter

---
+++ src/Components/FunctionCounter.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const FunctionCounter = () => {
+    return (
+        <div>
+            Funtion Counter
+        </div>
+    );
+}
+
+export default FunctionCounter;
