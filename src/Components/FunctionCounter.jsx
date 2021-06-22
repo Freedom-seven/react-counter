23dbde55d33f3181b02bd5cdf35da03f0e0fcb2a
FREEDOM <freewhynane62@gmail.com>
2021-06-22 13:43:47 +0000

    add state

--- src/Components/FunctionCounter.jsx
+++ src/Components/FunctionCounter.jsx
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
 
 const FunctionCounter = () => {
+    const [count, updateCount] = useState(0);
+
+    handleIncrease () {
+        
+    }
     return (
         <div>
-            Funtion Counter
+           <h1>Count: {count}</h1>
+           <hr/>
+           <button>Decrease</button>
+           <button>Increase</button>
         </div>
     );
 }
